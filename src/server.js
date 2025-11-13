4566692468f6e432cb3a80d82f3dc88cb262d5b7
Vladislav Popov <vlad_np@ukr.net>
2025-11-13 19:08:20 +0100

    debug mongoDB

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ import dotenv from "dotenv";
 dotenv.config();
 import app from "./app.js";
 import connectDB from "./config/db.js";
+import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 4000;
-
+console.log("🟢 Connecting to MongoDB URI:", process.env.MONGODB_URI);
+mongoose.set("debug", true);
 connectDB().then(() => {
   app.listen(PORT, () => console.log(`🚀 API running on port ${PORT}`));
 });
