498d0978e431f265427b4a4af82a2d3d0bd820f8
Vladislav Popov <vlad_np@ukr.net>
2025-11-13 19:54:51 +0100

    change connaction for mongodb

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 import dotenv from "dotenv";
-dotenv.config();
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import app from "./app.js";
-import connectDB from "./config/db.js";
 import mongoose from "mongoose";
 
-const PORT = process.env.PORT || 4000;
-console.log("🟢 Connecting to MongoDB URI:", process.env.MONGODB_URI);
-mongoose.set("debug", true);
-connectDB().then(() => {
-  app.listen(PORT, () => console.log(`🚀 API running on port ${PORT}`));
-});
+dotenv.config();
+
+// --- Виправлення для __dirname в ES Modules ---
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// ------------------------------------
+
+// Отримуємо змінні середовища
+const PORT = process.env.PORT || 3030;
+const mongoUri = process.env.MONGODB_URI;
+
+// Використовуйте стандартну змінну Node.js для визначення режиму
+const isProduction = process.env.NODE_ENV === "production";
+
+if (isProduction) {
+  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
+
+const connectDB = async () => {
+  if (!mongoUri) {
+    console.error("Помилка: MONGO_URI не визначено в .env файлі");
+    process.exit(1);
+  }
+
+  try {
+    const conn = await mongoose.connect(mongoUri, {});
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("✅ Database connection successful");
+      console.log(`🚀 Server running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
