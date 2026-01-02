a1e00ed4c3aea6f17ee9e75b13e3b343de7e0d49
Vladyslav Popov <vlad_np@ukr.net>
2026-01-02 15:59:12 +0000

    add public products routers

--- src/controllers/publicDetails.controller.js
+++ src/controllers/publicDetails.controller.js
@@ -31,3 +31,37 @@ export const getPublicSpecialistById = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
+
+import Product from "../models/product.model.js";
+
+export const getPublicProducts = async (req, res) => {
+  try {
+    let products = [];
+    const limit = parseInt(req.query.limit);
+    if (limit) {
+      products = await Product.find({ isActive: true })
+        .sort({ rating: -1 })
+        .limit(limit);
+    } else {
+      products = await Product.find({ isActive: true }).sort({
+        rating: -1,
+      });
+    }
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+/** READ ONE */
+export const getPublicProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.json(product);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
--- src/routes/publicDetails.routes.js
+++ src/routes/publicDetails.routes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import {
+  getPublicProductById,
+  getPublicProducts,
   getPublicSpecialistById,
   getPublicSpecialists,
 } from "../controllers/publicDetails.controller.js";
@@ -8,5 +10,7 @@ const router = express.Router();
 
 router.get("/specialists", getPublicSpecialists);
 router.get("/specialists/:id", getPublicSpecialistById);
+router.get("/products", getPublicProducts);
+router.get("/products/:id", getPublicProductById);
 
 export default router;
